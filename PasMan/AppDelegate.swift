7c4a3632610af06e193b5446c848a1c3a7a9bb54
Danil Ryumin <ryumin_danil@icloud.com>
2023-02-27 19:54:22 +0300

    Delete ViewController from storyboard

--- PasMan/AppDelegate.swift
+++ PasMan/AppDelegate.swift
@@ -1,19 +1,16 @@
-//
-//  AppDelegate.swift
-//  PasMan
-//
-//  Created by Danil Ryumin on 27.02.2023.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UINavigationController(rootViewController: HomeViewController())
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 
@@ -30,7 +27,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
