24a750f3da437d82e3b819c4623a8b023bcdca60
Danil Ryumin <ryumin_danil@icloud.com>
2023-03-17 20:00:54 +0300

    Update PasswordAddingVC design

--- PasMan/PasswordAddingViewController.swift
+++ PasMan/PasswordAddingViewController.swift
@@ -4,14 +4,16 @@ class PasswordAddingViewController: UIViewController {
     
     lazy var titleTextField: UITextField = {
         let textField = UITextField()
-        textField.placeholder = "Enter title here..."
-        textField.font = UIFont.systemFont(ofSize: 15)
-        textField.borderStyle = UITextField.BorderStyle.roundedRect
-        textField.autocorrectionType = UITextAutocorrectionType.no
-        textField.keyboardType = UIKeyboardType.default
-        textField.returnKeyType = UIReturnKeyType.done
-        textField.clearButtonMode = UITextField.ViewMode.whileEditing
-        textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        textField.placeholder = "Enter title here ..."
+        textField.font = UIFont(name: "Avenir Next Demi Bold", size: 15)
+        textField.backgroundColor = #colorLiteral(red: 0.3921568627, green: 0.5843137255, blue: 0.9294117647, alpha: 0.15)
+        textField.layer.borderColor = #colorLiteral(red: 0.3921568627, green: 0.5843137255, blue: 0.9294117647, alpha: 1)
+        textField.layer.borderWidth = 2
+        textField.layer.cornerRadius = 12
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
+        textField.autocorrectionType = .no
+        textField.returnKeyType = .done
         textField.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(textField)
         return textField
@@ -19,14 +21,16 @@ class PasswordAddingViewController: UIViewController {
     
     lazy var loginTextField: UITextField = {
         let textField = UITextField()
-        textField.placeholder = "Enter login here..."
-        textField.font = UIFont.systemFont(ofSize: 15)
-        textField.borderStyle = UITextField.BorderStyle.roundedRect
-        textField.autocorrectionType = UITextAutocorrectionType.no
-        textField.keyboardType = UIKeyboardType.default
-        textField.returnKeyType = UIReturnKeyType.done
-        textField.clearButtonMode = UITextField.ViewMode.whileEditing
-        textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        textField.placeholder = "Enter username here ..."
+        textField.font = UIFont(name: "Avenir Next Demi Bold", size: 15)
+        textField.backgroundColor = #colorLiteral(red: 0.3921568627, green: 0.5843137255, blue: 0.9294117647, alpha: 0.15)
+        textField.layer.borderColor = #colorLiteral(red: 0.3921568627, green: 0.5843137255, blue: 0.9294117647, alpha: 1)
+        textField.layer.borderWidth = 2
+        textField.layer.cornerRadius = 12
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
+        textField.autocorrectionType = .no
+        textField.returnKeyType = .done
         textField.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(textField)
         return textField
@@ -34,14 +38,16 @@ class PasswordAddingViewController: UIViewController {
     
     lazy var passwordTextField: UITextField = {
         let textField = UITextField()
-        textField.placeholder = "Enter password here..."
-        textField.font = UIFont.systemFont(ofSize: 15)
-        textField.borderStyle = UITextField.BorderStyle.roundedRect
-        textField.autocorrectionType = UITextAutocorrectionType.no
-        textField.keyboardType = UIKeyboardType.default
-        textField.returnKeyType = UIReturnKeyType.done
-        textField.clearButtonMode = UITextField.ViewMode.whileEditing
-        textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        textField.placeholder = "Enter title password ..."
+        textField.font = UIFont(name: "Avenir Next Demi Bold", size: 15)
+        textField.backgroundColor = #colorLiteral(red: 0.3921568627, green: 0.5843137255, blue: 0.9294117647, alpha: 0.15)
+        textField.layer.borderColor = #colorLiteral(red: 0.3921568627, green: 0.5843137255, blue: 0.9294117647, alpha: 1)
+        textField.layer.borderWidth = 2
+        textField.layer.cornerRadius = 12
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
+        textField.autocorrectionType = .no
+        textField.returnKeyType = .done
         textField.isSecureTextEntry = true
         textField.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(textField)
@@ -59,7 +65,33 @@ class PasswordAddingViewController: UIViewController {
         button.layer.shadowOpacity = 1
         button.layer.shadowRadius = 4
         button.layer.shadowOffset = CGSize(width: 1, height: 5)
-        button.addTarget(self, action: #selector(savePasswordButtonClick(sender:)), for: .touchUpInside)
+        
+        let action = UIAction { _ in
+            
+            guard let title = self.titleTextField.text,
+                  let login = self.loginTextField.text,
+                  let password = self.passwordTextField.text else { return }
+            
+            guard !title.isEmpty,
+                  !login.isEmpty,
+                  !password.isEmpty else {
+                
+                let alert = UIAlertController(title: "ERROR", message: "Fill in all the fields", preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK", style: .default)
+                alert.addAction(action)
+                
+                self.present(alert, animated: true)
+                return
+            }
+            
+            DataStoreManager.shared.createPasswordModel(title: title, login: login, password: password)
+            self.reloadDataDelegate?.reloadData()
+            self.updateNumberOfPasswordsLabelDelegate?.updateNumberOfPasswordsLabel()
+            
+            self.dismiss(animated: true)
+        }
+        button.addAction(action, for: .touchUpInside)
+        
         button.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(button)
         return button
@@ -67,69 +99,42 @@ class PasswordAddingViewController: UIViewController {
     
     var reloadDataDelegate: ReloadDataDelegate?
     var updateNumberOfPasswordsLabelDelegate: UpdateNumberOfPasswordsLabelDelegate?
-    private let dataStoreManager = DataStoreManager.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        setSaveButtonConstraints()
-        setTitleTextFieldConstraints()
-        setLoginTextFieldConstraints()
-        setPasswordTextFieldConstraints()
+        setupSaveButtonConstraints()
+        setupTitleTextFieldConstraints()
+        setupLoginTextFieldConstraints()
+        setupPasswordTextFieldConstraints()
     }
-    
-    @objc
-    func savePasswordButtonClick(sender: Any) {
-        
-        guard let title = titleTextField.text,
-              let login = loginTextField.text,
-              let password = passwordTextField.text else { return }
-        
-        guard !title.isEmpty,
-              !login.isEmpty,
-              !password.isEmpty else {
-            
-            let alert = UIAlertController(title: nil, message: "Fill in all the fields", preferredStyle: .alert)
-            let action = UIAlertAction(title: "OK", style: .default)
-            alert.addAction(action)
-            
-            present(alert, animated: true)
-            return
-        }
-        
-        dataStoreManager.createPasswordModel(title: title, login: login, password: password)
-        reloadDataDelegate?.reloadData()
-        updateNumberOfPasswordsLabelDelegate?.updateNumberOfPasswordsLabel()
-        
-        dismiss(animated: true)
-    }
-    
-    func setSaveButtonConstraints() {
+
+    func setupSaveButtonConstraints() {
+        saveButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 60).isActive = true
         saveButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         saveButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
-        saveButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 60).isActive = true
         saveButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     
-    func setTitleTextFieldConstraints() {
+    func setupTitleTextFieldConstraints() {
+        titleTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
         titleTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         titleTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
-        titleTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
         titleTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     
-    func setLoginTextFieldConstraints() {
+    func setupLoginTextFieldConstraints() {
+        loginTextField.topAnchor.constraint(equalTo: titleTextField.bottomAnchor, constant: 5).isActive = true
         loginTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         loginTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
-        loginTextField.topAnchor.constraint(equalTo: titleTextField.bottomAnchor, constant: 5).isActive = true
         loginTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     
-    func setPasswordTextFieldConstraints() {
+    func setupPasswordTextFieldConstraints() {
+        passwordTextField.topAnchor.constraint(equalTo: loginTextField.bottomAnchor, constant: 5).isActive = true
         passwordTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         passwordTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
-        passwordTextField.topAnchor.constraint(equalTo: loginTextField.bottomAnchor, constant: 5).isActive = true
         passwordTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
 }
