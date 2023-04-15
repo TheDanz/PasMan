c24edae62375476e9c7ce1aea795d8fee31b9213
TheDanz <ryumin_danil@icloud.com>
2023-04-15 20:57:53 +0300

    Add passwordTextView on PasswordGenerationVC

--- PasMan/Controllers/PasswordGenerationViewController.swift
+++ PasMan/Controllers/PasswordGenerationViewController.swift
@@ -1,9 +1,41 @@
 import UIKit
 
 class PasswordGenerationViewController: UIViewController {
+    
+    lazy var passwordTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont(name: "Avenir Next Bold", size: 26)
+        textView.text = "YOUR STRONG PASSWORD"
+        textView.textColor = #colorLiteral(red: 0.3921568627, green: 0.5843137255, blue: 0.9294117647, alpha: 1)
+        textView.layer.cornerRadius = 12
+        textView.textContainer.maximumNumberOfLines = 1
+        textView.textContainer.lineBreakMode = .byTruncatingTail
+        textView.isScrollEnabled = false
+        textView.isEditable = false
+        textView.textAlignment = .center
+        textView.backgroundColor = .systemGray5
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(textView)
+        return textView
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+    
+        setPasswordTextViewConstraints()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        passwordTextView.alignVerticalCenter()
+    }
+    
+    func setPasswordTextViewConstraints() {
+        passwordTextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        passwordTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
+        passwordTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
+        passwordTextView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2).isActive = true
     }
 }
