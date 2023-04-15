4e333503945958645eeeea78ebbb76c8d8ad7b14
TheDanz <ryumin_danil@icloud.com>
2023-04-15 17:08:33 +0300

    Create PasswordGenerationclass and function

---
+++ PasMan/PasswordGeneration.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+final class PasswordGeneration {
+    
+    enum CharacterSet: String {
+        case upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        case lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+        case numbers = "0123456789"
+        case specialCharacters = "!@#$%^&*()_+-=[]{}|;':\"<>,.?\\/"
+    }
+    
+    func generatePassword(length: Int, using: Set<CharacterSet>) -> String {
+        
+        var password = ""
+        var characters: [Character] = []
+        
+        for set in using {
+            characters.append(contentsOf: Array(set.rawValue))
+        }
+        
+        for _ in 0..<length {
+            let randomIndex = Int(arc4random_uniform(UInt32(characters.count)))
+            password += String(characters[randomIndex])
+        }
+        
+        return password
+    }
+}
