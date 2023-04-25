ddc6d4780fc895e69d8cb2b7aeca48d1d820e3dd
TheDanz <ryumin_danil@icloud.com>
2023-04-25 17:15:37 +0300

    Add WKWebView

--- PasMan/Controllers/InformationViewController.swift
+++ PasMan/Controllers/InformationViewController.swift
@@ -1,9 +1,22 @@
 import UIKit
+import WebKit
 
-class InformationViewController: UIViewController {
+class InformationViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
+        let url = URL(string: "https://github.com/TheDanz/PasMan")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 }
