0d388877405a000169cbfb0c84fb19f878758ac0
TheDanz <ryumin_danil@icloud.com>
2023-04-25 17:29:33 +0300

    Add activityIndicatorView

--- PasMan/Controllers/InformationViewController.swift
+++ PasMan/Controllers/InformationViewController.swift
@@ -1,9 +1,20 @@
 import UIKit
 import WebKit
 
-class InformationViewController: UIViewController, WKNavigationDelegate {
+class InformationViewController: UIViewController {
     
     var webView: WKWebView!
+    
+    lazy var activityIndicatorView: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView()
+        indicator.startAnimating()
+        indicator.hidesWhenStopped = true
+        indicator.color = #colorLiteral(red: 0.3921568627, green: 0.5843137255, blue: 0.9294117647, alpha: 1)
+        indicator.transform = CGAffineTransform(scaleX: 2, y: 2)
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        self.webView.addSubview(indicator)
+        return indicator
+    }()
     
     override func loadView() {
         webView = WKWebView()
@@ -18,5 +29,23 @@ class InformationViewController: UIViewController, WKNavigationDelegate {
         let url = URL(string: "https://github.com/TheDanz/PasMan")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+        
+        setupActivityIndicatorViewConstraints()
+    }
+    
+    func setupActivityIndicatorViewConstraints() {
+        activityIndicatorView.centerXAnchor.constraint(equalTo: webView.centerXAnchor).isActive = true
+        activityIndicatorView.centerYAnchor.constraint(equalTo: webView.centerYAnchor).isActive = true
+    }
+}
+
+extension InformationViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        activityIndicatorView.stopAnimating()
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        activityIndicatorView.stopAnimating()
     }
 }
