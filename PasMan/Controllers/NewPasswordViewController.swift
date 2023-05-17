46d6a6d72bc95556de186a5dd46f5c7bbfa1d07f
TheDanz <ryumin_danil@icloud.com>
2023-05-17 22:35:23 +0300

    Create lifeTimeView and stepperView on NewPasswordVC

--- PasMan/Controllers/NewPasswordViewController.swift
+++ PasMan/Controllers/NewPasswordViewController.swift
@@ -54,6 +54,28 @@ class NewPasswordViewController: UIViewController {
         return textField
     }()
     
+    lazy var lifeTimeView: LabelSwitchView = {
+        let labelSwitchView = LabelSwitchView()
+        labelSwitchView.leftLabel.text = "Add password lifetime?".localized()
+        labelSwitchView.rightSwitch.addTarget(self, action: #selector(lifeTimeSwitchChangedValue), for: .valueChanged)
+        labelSwitchView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(labelSwitchView)
+        return labelSwitchView
+    }()
+    
+    lazy var stepperView: LabelStepperView = {
+        let labelStepperView = LabelStepperView()
+        labelStepperView.isHidden = true
+        labelStepperView.rightStepper.minimumValue = 1
+        labelStepperView.rightStepper.maximumValue = 360
+        labelStepperView.rightStepper.stepValue = 1
+        labelStepperView.leftLabel.text = "Amount of days".localized() + ": 1"
+        labelStepperView.rightStepper.addTarget(self, action: #selector(stepperChangedValue), for: .valueChanged)
+        labelStepperView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(labelStepperView)
+        return labelStepperView
+    }()
+    
     lazy var saveButton: UIButton = {
         let button = UIButton()
         button.setTitle("Save password".localized(), for: .normal)
@@ -102,37 +124,75 @@ class NewPasswordViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         
-        setupSaveButtonConstraints()
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
+        self.view.addGestureRecognizer(tapGesture)
+        
+        setupAllConstraints()
+    }
+    
+    @objc
+    private func lifeTimeSwitchChangedValue(_ sender: UISwitch) {
+        stepperView.isHidden = stepperView.isHidden ? false : true
+    }
+    
+    @objc
+    private func stepperChangedValue(_ sender: UIStepper) {
+        stepperView.leftLabel.text = "Amount of days".localized() + ": " + String(Int(stepperView.rightStepper.value))
+    }
+    
+    @objc
+    private func dismissKeyboard (_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    private func setupAllConstraints() {
         setupTitleTextFieldConstraints()
         setupLoginTextFieldConstraints()
         setupPasswordTextFieldConstraints()
-    }
-
-    func setupSaveButtonConstraints() {
-        saveButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 60).isActive = true
-        saveButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
-        saveButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
-        saveButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        setupLifeTimeViewConstraints()
+        setupSaveButtonConstraints()
+        setupStepperViewConstraints()
     }
     
-    func setupTitleTextFieldConstraints() {
+    private func setupTitleTextFieldConstraints() {
         titleTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
         titleTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         titleTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
         titleTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     
-    func setupLoginTextFieldConstraints() {
+    private func setupLoginTextFieldConstraints() {
         loginTextField.topAnchor.constraint(equalTo: titleTextField.bottomAnchor, constant: 5).isActive = true
         loginTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         loginTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
         loginTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     
-    func setupPasswordTextFieldConstraints() {
+    private func setupPasswordTextFieldConstraints() {
         passwordTextField.topAnchor.constraint(equalTo: loginTextField.bottomAnchor, constant: 5).isActive = true
         passwordTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         passwordTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
         passwordTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    private func setupLifeTimeViewConstraints() {
+        lifeTimeView.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 30).isActive = true
+        lifeTimeView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        lifeTimeView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        lifeTimeView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    private func setupStepperViewConstraints() {
+        stepperView.topAnchor.constraint(equalTo: lifeTimeView.bottomAnchor, constant: 5).isActive = true
+        stepperView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        stepperView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        stepperView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    private func setupSaveButtonConstraints() {
+        saveButton.topAnchor.constraint(equalTo: stepperView.bottomAnchor, constant: 30).isActive = true
+        saveButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        saveButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        saveButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
 }
