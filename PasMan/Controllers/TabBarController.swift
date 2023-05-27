629273a13e536ebad1578d96762148a529d19917
TheDanz <ryumin_danil@icloud.com>
2023-05-27 19:26:49 +0300

    Add highlight of current tab bar item

--- PasMan/Controllers/TabBarController.swift
+++ PasMan/Controllers/TabBarController.swift
@@ -29,6 +29,12 @@ class TabBarController: UITabBarController {
         tabBar.unselectedItemTintColor = #colorLiteral(red: 0.3921568627, green: 0.5843137255, blue: 0.9294117647, alpha: 1)
         tabBar.backgroundColor = #colorLiteral(red: 0.3921568627, green: 0.5843137255, blue: 0.9294117647, alpha: 0.2)
         
+        setDefaultTabBarItemImages()
+        setTabBarItemImage(index: 0, systemName: "house.circle.fill")
+    }
+    
+    private func setDefaultTabBarItemImages() {
+        
         if let items = tabBar.items {
             
             let images = ["house.circle", "123.rectangle", "lock.circle"]
@@ -39,20 +45,36 @@ class TabBarController: UITabBarController {
             }
         }
     }
+    
+    
+    private func setTabBarItemImage(index: Int, systemName: String) {
+        
+        let config = UIImage.SymbolConfiguration(pointSize: 20)
+        
+        if let items = tabBar.items {
+            items[index].image = UIImage(systemName: systemName, withConfiguration: config)
+        }
+    }
 }
 
 extension TabBarController: UITabBarControllerDelegate {
     func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        
+        setDefaultTabBarItemImages()
         
         let tabBarIndex = tabBarController.selectedIndex
         switch tabBarIndex {
         case 0:
             self.homeVC.updateLeftExpirationViewText()
             self.homeVC.updateRightWeaknessViewText()
+            self.setTabBarItemImage(index: 0, systemName: "house.circle.fill")
+        case 1:
+            self.setTabBarItemImage(index: 1, systemName: "123.rectangle.fill")
         case 2:
             DispatchQueue.main.async {
                 self.passwordTVC.passwordsTableView.reloadData()
             }
+            self.setTabBarItemImage(index: 2, systemName: "lock.circle.fill")
         default:
             break
         }
